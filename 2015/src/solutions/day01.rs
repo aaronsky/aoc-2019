5f33d200f013e0c4af75106a9fee7c5ddeb296fa
Aaron Sky <aaronsky@skyaaron.com>
2020-12-03 17:08:24 -0500

    Added 2015, for fun

---
+++ 2015/src/solutions/day01.rs
@@ -0,0 +1,40 @@
+pub fn get_floor_number(pattern: &str) -> i32 {
+    let mut current_floor = 0;
+    for p in pattern.chars() {
+        match p {
+            '(' => current_floor += 1,
+            ')' => current_floor -= 1,
+            _ => unreachable!("impossible value"),
+        }
+    }
+    current_floor
+}
+
+pub fn get_index_for_floor_number(pattern: &str, target: i32) -> usize {
+    let mut current_floor = 0;
+    for (i, p) in pattern.chars().enumerate() {
+        match p {
+            '(' => current_floor += 1,
+            ')' => current_floor -= 1,
+            _ => unreachable!("impossible value"),
+        }
+        if current_floor == target {
+            return i + 1;
+        }
+    }
+
+    usize::MAX
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use util;
+
+    #[test]
+    fn test_advent_puzzle() {
+        let input: String = util::load_input_file("day01.txt").unwrap();
+        assert_eq!(get_floor_number(&input), 232);
+        assert_eq!(get_index_for_floor_number(&input, -1), 1783);
+    }
+}
