ebf642e344bc9394f725c66db19e0dd19e2eb88c
Aaron Sky <aaronsky@skyaaron.com>
2020-12-09 20:00:13 -0500

    stage day 10 2020 solution file

---
+++ 2020/src/solutions/day10.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use util;
+
+    #[test]
+    fn test_advent_puzzle() {
+        let _input = util::Input::new("day10.txt", crate::YEAR).unwrap();
+    }
+}
