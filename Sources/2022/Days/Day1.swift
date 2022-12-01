5ffb8da2c2abeaa22a727b93e0500c25c6e6b645
Aaron Sky <aaronsky@skyaaron.com>
2022-12-01 00:14:44 -0500

    2022 day 1

--- Sources/2022/Days/Day1.swift
+++ Sources/2022/Days/Day1.swift
@@ -2,13 +2,42 @@ import Algorithms
 import Base
 
 struct Day1: Day {
-    init(_ input: Input) throws {}
+    var elves: [LunchBox]
+
+    init(_ input: Input) throws {
+        self.elves = []
+
+        var boxes: [Int] = []
+        for line in input.raw.components(separatedBy: "\n") {
+            if line.isEmpty {
+                elves.append(.init(boxes: boxes))
+                boxes = []
+            } else if let calories = Int(line) {
+                boxes.append(calories)
+            }
+        }
+    }
 
     func partOne() async -> String {
-        ""
+        "\(elves.max(by: { $0.sum < $1.sum })!.sum)"
     }
 
     func partTwo() async -> String {
-        ""
+        "\(elves.max(count: 3, sortedBy: { $0.sum < $1.sum }).sum(of: \.sum))"
+    }
+
+    struct LunchBox {
+        var boxes: [Int] {
+            didSet {
+                sum = boxes.reduce(0, +)
+            }
+        }
+
+        var sum: Int = 0
+
+        init(boxes: [Int]) {
+            self.boxes = boxes
+            self.sum = boxes.reduce(0, +)
+        }
     }
 }
--- Tests/2022Tests/Day1Tests.swift
+++ Tests/2022Tests/Day1Tests.swift
@@ -7,8 +7,8 @@ class Day1Tests: XCTestCase {
     func testProblems() async throws {
         let day = try await Year2022().day(for: 1)
         let partOne = await day.partOne()
-        XCTAssertEqual(partOne, "")
+        XCTAssertEqual(partOne, "65912")
         let partTwo = await day.partTwo()
-        XCTAssertEqual(partTwo, "")
+        XCTAssertEqual(partTwo, "195625")
     }
 }
