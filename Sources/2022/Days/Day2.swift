7b033137d9114ea5eaffbcafd335de114a29e711
Aaron Sky <aaronsky@skyaaron.com>
2022-12-02 00:50:35 -0500

    day 2 tweaks

--- Sources/2022/Days/Day2.swift
+++ Sources/2022/Days/Day2.swift
@@ -2,122 +2,63 @@ import Algorithms
 import Base
 
 struct Day2: Day {
-    let games: [(Move, Play)]
+    /// A and X are Rock, Rock is worth 1
+    /// B and Y are Paper, Paper is worth 2
+    /// C and Z are Scissors, Scissors are worth 3
+    private let versusOutcomes: [String: [String: Int]] = [
+        "A": [
+            "X": 4,
+            "Y": 8,
+            "Z": 3,
+        ],
+        "B": [
+            "X": 1,
+            "Y": 5,
+            "Z": 9,
+        ],
+        "C": [
+            "X": 7,
+            "Y": 2,
+            "Z": 6,
+        ],
+    ]
+
+    /// X is Lose
+    /// Y is Tie
+    /// Z is Win
+    private let predictiveOutcomes: [String: [String: Int]] = [
+        "A": [
+            "X": 3,
+            "Y": 4,
+            "Z": 8,
+        ],
+        "B": [
+            "X": 1,
+            "Y": 5,
+            "Z": 9,
+        ],
+        "C": [
+            "X": 2,
+            "Y": 6,
+            "Z": 7,
+        ],
+    ]
+
+    var moves: [(String, String)]
 
     init(_ input: Input) throws {
-        games = input.lines.compactMap {
+        moves = input.lines.compactMap {
             let moves = $0.components(separatedBy: " ")
             guard moves.count == 2 else { return nil }
-            return (Move(moves[0]), Play(moves[1]))
+            return (moves[0], moves[1])
         }
     }
 
     func partOne() async -> String {
-        let score = games.sum(of: { moves in
-            let outcome: Outcome
-            switch (moves.0, moves.1) {
-            case (.a, .x):
-                outcome = .draw
-            case (.a, .y):
-                outcome = .win
-            case (.a, .z):
-                outcome = .lose
-            case (.b, .x):
-                outcome = .lose
-            case (.b, .y):
-                outcome = .draw
-            case (.b, .z):
-                outcome = .win
-            case (.c, .x):
-                outcome = .win
-            case (.c, .y):
-                outcome = .lose
-            case (.c, .z):
-                outcome = .draw
-            }
-
-            return moves.1.rawValue + outcome.rawValue
-        })
-
-        return "\(score)"
+        "\(moves.sum(of: { versusOutcomes[$0.0]![$0.1]! }))"
     }
 
     func partTwo() async -> String {
-        let score = games.sum(of: { moves in
-            let outcome: Outcome
-            let necessaryMove: Move
-
-            switch moves.1 {
-            case .x:
-                outcome = .lose
-            case .y:
-                outcome = .draw
-            case .z:
-                outcome = .win
-            }
-
-            switch (moves.0, outcome) {
-            case (.a, .lose):
-                necessaryMove = .c
-            case (.a, .draw):
-                necessaryMove = .a
-            case (.a, .win):
-                necessaryMove = .b
-            case (.b, .lose):
-                necessaryMove = .a
-            case (.b, .draw):
-                necessaryMove = .b
-            case (.b, .win):
-                necessaryMove = .c
-            case (.c, .lose):
-                necessaryMove = .b
-            case (.c, .draw):
-                necessaryMove = .c
-            case (.c, .win):
-                necessaryMove = .a
-            }
-
-            return necessaryMove.rawValue + outcome.rawValue
-        })
-
-        return "\(score)"
-    }
-
-    enum Outcome: Int {
-        case lose = 0, draw = 3, win = 6
-    }
-
-    enum Move: Int {
-        case a = 1, b, c
-
-        init(_ rawValue: String) {
-            switch rawValue {
-            case "A", "X":
-                self = .a
-            case "B", "Y":
-                self = .b
-            case "C", "Z":
-                self = .c
-            default:
-                fatalError()
-            }
-        }
-    }
-
-    enum Play: Int {
-        case x = 1, y, z
-
-        init(_ rawValue: String) {
-            switch rawValue {
-            case "X":
-                self = .x
-            case "Y":
-                self = .y
-            case "Z":
-                self = .z
-            default:
-                fatalError()
-            }
-        }
+        "\(moves.sum(of: { predictiveOutcomes[$0.0]![$0.1]! }))"
     }
 }
