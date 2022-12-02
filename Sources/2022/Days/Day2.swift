9f7e27a10cf2acc0785e38a9cade81061423206a
Aaron Sky <aaronsky@skyaaron.com>
2022-12-02 00:33:38 -0500

    2022 day 2 initial

--- Sources/2022/Days/Day2.swift
+++ Sources/2022/Days/Day2.swift
@@ -2,13 +2,122 @@ import Algorithms
 import Base
 
 struct Day2: Day {
-    init(_ input: Input) throws {}
+    let games: [(Move, Play)]
+
+    init(_ input: Input) throws {
+        games = input.lines.compactMap {
+            let moves = $0.components(separatedBy: " ")
+            guard moves.count == 2 else { return nil }
+            return (Move(moves[0]), Play(moves[1]))
+        }
+    }
 
     func partOne() async -> String {
-        ""
+        let score = games.sum(of: { moves in
+            let outcome: Outcome
+            switch (moves.0, moves.1) {
+            case (.a, .x):
+                outcome = .draw
+            case (.a, .y):
+                outcome = .win
+            case (.a, .z):
+                outcome = .lose
+            case (.b, .x):
+                outcome = .lose
+            case (.b, .y):
+                outcome = .draw
+            case (.b, .z):
+                outcome = .win
+            case (.c, .x):
+                outcome = .win
+            case (.c, .y):
+                outcome = .lose
+            case (.c, .z):
+                outcome = .draw
+            }
+
+            return moves.1.rawValue + outcome.rawValue
+        })
+
+        return "\(score)"
     }
 
     func partTwo() async -> String {
-        ""
+        let score = games.sum(of: { moves in
+            let outcome: Outcome
+            let necessaryMove: Move
+
+            switch moves.1 {
+            case .x:
+                outcome = .lose
+            case .y:
+                outcome = .draw
+            case .z:
+                outcome = .win
+            }
+
+            switch (moves.0, outcome) {
+            case (.a, .lose):
+                necessaryMove = .c
+            case (.a, .draw):
+                necessaryMove = .a
+            case (.a, .win):
+                necessaryMove = .b
+            case (.b, .lose):
+                necessaryMove = .a
+            case (.b, .draw):
+                necessaryMove = .b
+            case (.b, .win):
+                necessaryMove = .c
+            case (.c, .lose):
+                necessaryMove = .b
+            case (.c, .draw):
+                necessaryMove = .c
+            case (.c, .win):
+                necessaryMove = .a
+            }
+
+            return necessaryMove.rawValue + outcome.rawValue
+        })
+
+        return "\(score)"
+    }
+
+    enum Outcome: Int {
+        case lose = 0, draw = 3, win = 6
+    }
+
+    enum Move: Int {
+        case a = 1, b, c
+
+        init(_ rawValue: String) {
+            switch rawValue {
+            case "A", "X":
+                self = .a
+            case "B", "Y":
+                self = .b
+            case "C", "Z":
+                self = .c
+            default:
+                fatalError()
+            }
+        }
+    }
+
+    enum Play: Int {
+        case x = 1, y, z
+
+        init(_ rawValue: String) {
+            switch rawValue {
+            case "X":
+                self = .x
+            case "Y":
+                self = .y
+            case "Z":
+                self = .z
+            default:
+                fatalError()
+            }
+        }
     }
 }
--- Tests/2022Tests/Day2Tests.swift
+++ Tests/2022Tests/Day2Tests.swift
@@ -7,8 +7,8 @@ class Day2Tests: XCTestCase {
     func testProblems() async throws {
         let day = try await Year2022().day(for: 2)
         let partOne = await day.partOne()
-        XCTAssertEqual(partOne, "")
+        XCTAssertEqual(partOne, "12586")
         let partTwo = await day.partTwo()
-        XCTAssertEqual(partTwo, "")
+        XCTAssertEqual(partTwo, "13193")
     }
 }
