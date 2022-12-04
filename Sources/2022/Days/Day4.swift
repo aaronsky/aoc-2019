119cbfe15d864e92b06313c313df74820ea645b0
Aaron Sky <aaronsky@skyaaron.com>
2022-12-04 00:16:05 -0500

    2022 day 4

--- Sources/2022/Days/Day4.swift
+++ Sources/2022/Days/Day4.swift
@@ -1,14 +1,43 @@
 import Algorithms
 import Base
+import RegexBuilder
 
 struct Day4: Day {
-    init(_ input: Input) throws {}
+    var indexPairs: [(ClosedRange<Int>, ClosedRange<Int>)]
+
+    init(_ input: Input) throws {
+        let min1Ref = Reference<Int>()
+        let max1Ref = Reference<Int>()
+        let min2Ref = Reference<Int>()
+        let max2Ref = Reference<Int>()
+        let pattern = Regex {
+            TryCapture(OneOrMore(.digit), as: min1Ref, transform: {
+                Int($0)
+            })
+            "-"
+            TryCapture(OneOrMore(.digit), as: max1Ref, transform: {
+                Int($0)
+            })
+            ","
+            TryCapture(OneOrMore(.digit), as: min2Ref, transform: {
+                Int($0)
+            })
+            "-"
+            TryCapture(OneOrMore(.digit), as: max2Ref, transform: {
+                Int($0)
+            })
+        }
+        indexPairs = input.decodeMany(separatedBy: "\n") { line in
+            guard let match = try? pattern.firstMatch(in: line) else { return nil }
+            return (match[min1Ref]...match[max1Ref], match[min2Ref]...match[max2Ref])
+        }
+    }
 
     func partOne() async -> String {
-        ""
+        "\(indexPairs.count(where: { $0.0.contains($0.1) || $0.1.contains($0.0) }))"
     }
 
     func partTwo() async -> String {
-        ""
+        "\(indexPairs.count(where: { $0.0.overlaps($0.1) || $0.1.overlaps($0.0) }))"
     }
 }
--- Tests/2022Tests/Day4Tests.swift
+++ Tests/2022Tests/Day4Tests.swift
@@ -7,8 +7,8 @@ class Day4Tests: XCTestCase {
     func testProblems() async throws {
         let day = try await Year2022().day(for: 4)
         let partOne = await day.partOne()
-        XCTAssertEqual(partOne, "")
+        XCTAssertEqual(partOne, "413")
         let partTwo = await day.partTwo()
-        XCTAssertEqual(partTwo, "")
+        XCTAssertEqual(partTwo, "806")
     }
 }
