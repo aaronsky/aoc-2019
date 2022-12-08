e3c13b6691d6ceea6c9f763480159b19f00ec1d5
Aaron Sky <aaronsky@skyaaron.com>
2022-12-07 21:48:06 -0800

    2022 day 8

--- Sources/2022/Days/Day8.swift
+++ Sources/2022/Days/Day8.swift
@@ -2,13 +2,66 @@ import Algorithms
 import Base
 
 struct Day8: Day {
-    init(_ input: Input) throws {}
+    var grid: Matrix<Int>
+    let directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    init(_ input: Input) throws {
+        grid = Matrix(input.lines.map { $0.map { Int(String($0))! } })
+    }
 
     func partOne() async -> String {
-        ""
+        var totalVisible = 0
+
+        for index in grid.indices {
+            let (x, y) = grid.position(of: index)
+            for (dx, dy) in directions {
+                var nx = x + dx
+                var ny = y + dy
+
+                while (0..<grid.rows).contains(nx) && (0..<grid.rowWidth).contains(ny) && grid[nx, ny] < grid[x, y] {
+                    nx += dx
+                    ny += dy
+                }
+
+                guard (0..<grid.rows).contains(nx) && (0..<grid.rowWidth).contains(ny) else {
+                    totalVisible += 1
+                    break
+                }
+            }
+        }
+
+        return "\(totalVisible)"
     }
 
     func partTwo() async -> String {
-        ""
+        var totalVisible = 0
+
+        for index in grid.indices {
+            let (x, y) = grid.position(of: index)
+            var score = 1
+
+            for (dx, dy) in directions {
+                var current = 0
+                var nx = x + dx
+                var ny = y + dy
+
+                while (0..<grid.rows).contains(nx) && (0..<grid.rowWidth).contains(ny) {
+                    current += 1
+
+                    if grid[nx, ny] >= grid[x, y] {
+                        break
+                    }
+
+                    nx += dx
+                    ny += dy
+                }
+
+                score *= current
+            }
+
+            totalVisible = max(totalVisible, score)
+        }
+
+        return "\(totalVisible)"
     }
 }
--- Tests/2022Tests/Day8Tests.swift
+++ Tests/2022Tests/Day8Tests.swift
@@ -7,8 +7,8 @@ class Day8Tests: XCTestCase {
     func testProblems() async throws {
         let day = try await Year2022().day(for: 8)
         let partOne = await day.partOne()
-        XCTAssertEqual(partOne, "")
+        XCTAssertEqual(partOne, "1859")
         let partTwo = await day.partTwo()
-        XCTAssertEqual(partTwo, "")
+        XCTAssertEqual(partTwo, "332640")
     }
 }
